f126525a3b498c04acdeb9747258018975f78f41
Minttu Linjala <cerealley@gmail.com>
2018-09-19 00:23:26 +0900

    Remove wave animation Add letterboxing support Fix cat not pressing anything if both keys pressed and one released

--- renderer.js
+++ renderer.js
@@ -5,6 +5,10 @@ const resolution = {
 	x: config.resX,
 	y: config.resY
 };
+const letterbox = {
+	x: config.lbResX || 0,
+	y: config.lbResY || 0
+}
 let device = (function(device){
 		switch(device){
 			case true:
@@ -35,34 +39,6 @@ leftup.src = "images/left_up.png"
 base.onload = () => {
 	ctx.drawImage(base, 0, 0);
 };
-let waves = 0;
-let waveSide = 0;
-function wave(){
-	if(waves < 11){
-		let left = new Image();
-		left.src = "images/wave_left.png";
-		let right = new Image();
-		right.src = "images/wave_right.png";
-		window.requestAnimationFrame(wave);
-		if (waveSide == 0) {
-			for(let j=0; j<12000; j++){
-				ctx.drawImage(left,0,0);
-			}
-			waveSide = 1;
-		}
-		else{
-			for(let j=0; j<12000; j++){
-				ctx.drawImage(right,0,0);
-			}
-			waveSide = 0;
-		}
-		waves++;
-	}
-	else{
-		ctx.drawImage(base, 0, 0);
-		start();
-	}
-}
 function start(){
 	let image = new Image();
 	image.src = config.mouse ? "images/devices_mouse.png" : "images/devices.png"
@@ -95,23 +71,55 @@ function start(){
 			drawIt();
 	});
 };
-wave();
+start();
+let keysDown = {
+	left: false,
+	right: false
+};
 ioHook.on("keydown", event => {
-	if(event.rawcode == config.keyLeft)
+	if(event.rawcode == config.keyLeft){
 		ctx.drawImage(pressLeft, 600, 320);
-	else if(event.rawcode == config.keyRight)
+		keysDown.left = true;
+	}
+	else if(event.rawcode == config.keyRight){
 		ctx.drawImage(pressRight, 600, 320);
+		keysDown.right = true;
+	}
 });
 ioHook.on("keyup", event => {
-	ctx.drawImage(leftup, 600, 320);
+	if(event.rawcode == config.keyLeft){
+		keysDown.left = false;
+	}
+	if(event.rawcode == config.keyRight){
+		keysDown.right = false;
+	}
+	if(!keysDown.left && !keysDown.right){
+		ctx.drawImage(leftup, 600, 320);
+	}
 });
 function moveCursor(event){
-	if(event.x < 0) event.x = 0;
-	if(event.x > resolution.x || event.x == resolution.x) event.x = resolution.x-1;
-	if(event.y < 0) event.y = 0;
-	if(event.y > resolution.y || event.y == resolution.y) event.y = resolution.y-1;
-	let x = Math.abs(Math.floor(Number(event.x)/(Number(resolution.x)/3)));
-	let y = Math.abs(Math.floor(Number(event.y)/(Number(resolution.y)/3)));
+	let x, y;
+	if(config.letterboxing){
+		let offsetX = ( resolution.x - letterbox.x ) / 2;
+		let offsetY = ( resolution.y - letterbox.y ) / 2;
+		let posX, posY;
+		posX = event.x - offsetX;
+		posY = event.y - offsetY;
+		if(posX < 0) posX = 0;
+		if(posX > letterbox.x || posX == letterbox.x) posX = letterbox.x-1;
+		if(posY < 0) posY = 0;
+		if(posY > letterbox.y || posY == letterbox.y) posY = letterbox.y-1;
+		x = Math.floor(Number(posX)/(Number(letterbox.x)/3));
+		y = Math.floor(Number(posY)/(Number(letterbox.y)/3));
+	}
+	else{
+		if(event.x < 0) event.x = 0;
+		if(event.x > resolution.x || event.x == resolution.x) event.x = resolution.x-1;
+		if(event.y < 0) event.y = 0;
+		if(event.y > resolution.y || event.y == resolution.y) event.y = resolution.y-1;
+		x = Math.floor(Number(event.x)/(Number(resolution.x)/3));
+		y = Math.floor(Number(event.y)/(Number(resolution.y)/3));
+	}
 	let field = x + y * 3;
 
 	if(field !== currentField && field !== undefined){
@@ -124,6 +132,3 @@ function moveCursor(event){
 ioHook.on("mousemove", event => moveCursor(event));
 ioHook.on("mousedrag", event => moveCursor(event));
 ioHook.start();
-
-//600,320 left hand
-//256,261 / 256,240 right hand, size 242x239
